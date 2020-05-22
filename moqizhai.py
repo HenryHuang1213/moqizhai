4c4d99a11b7dd7a7e0e751054a03da19d6b2ca83
HenryHuang1213 <henryhuang1213@gmail.com>
2020-05-22 10:23:13 +0800

    ready for CI

--- moqizhai.py
+++ moqizhai.py
@@ -13,5 +13,5 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',debug=app.config['DEBUG'],port=8000)
+    app.run(host='0.0.0.0',debug=app.config['DEBUG'],port=8000)
 
